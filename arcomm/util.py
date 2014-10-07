4c5c371178e098df9b5aacb9bdebdb509e0bf5b4
Jesse Mather <jrmather@gmail.com>
2014-10-07 10:29:12 -0700

    only convert strings, lists or tuples to lists...

--- arcomm/util.py
+++ arcomm/util.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """Utility helper functions and shortcuts"""
 
+
 def to_list(data):
     """Creates a list containing the data as a single element or a new list
-    from the original if it is already an iterable"""
-
-    if not hasattr(data, "__iter__"):
-        data = [] if data is None else [data]
-    return list(data)
+    from the original if it is already a list or a tuple"""
+    if isinstance(data, (list, tuple)):
+        return list(data)
+    else:
+        return [data]
 
 def to_multiline_string(data, end_of_line="\r\n"):
     """Return a string from a list"""
@@ -16,3 +17,15 @@ def to_multiline_string(data, end_of_line="\r\n"):
         data = end_of_line.join(data)
 
     return data
+
+def to_list_of_commands(commands):
+    """Converis a command or list of commands (strings) to a list of command
+    objects"""
+    commands = to_list(commands)
+    list_of_commmands = []
+    for _cmd in commands:
+        from command import Command
+        if not isinstance(_cmd, Command):
+            _cmd = Command(_cmd)
+        list_of_commmands.append(_cmd)
+    return list_of_commmands
