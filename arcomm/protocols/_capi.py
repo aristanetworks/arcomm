ef6a8887b6d4817cff756b6f97fcf431144771e3
Jesse Mather <jrmather@gmail.com>
2014-10-23 23:22:11 -0700

    new capi module

---
+++ arcomm/protocols/_capi.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""CAPI module to replace need for jsonrpclib"""
+import json
+import urllib2
+
+class _CapiException(Exception):
+    pass
+
+class _Capi(object):
+    """Simple implementation of a CAPI client that does not use jsonrpclib"""
+
+    #pylint: disable=too-many-instance-attributes
+    __enable = __encoding = __protocol = __timestamps = __uri = None
+
+    def __init__(self, host, username, password="", enable="", use_ssl=False,
+                 port=None, encoding="json", timestamps=False):
+        #pylint: disable=too-many-arguments
+        self.capi_id = "arcomm.protocols." + self.__class__.__name__
+        self.host = host
+        self.user = username
+        self.password = password
+        self.enable_pass = enable
+        self.encoding = encoding
+        self.timestamps = timestamps
+        self.protocol = "http" if not use_ssl else "https"
+        self.port = port or (80 if self.protocol == "http" else 443)
+        self.http = self._get_auth_opener()
+
+    @property
+    def encoding(self):
+        #pylint: disable=missing-docstring
+        return self.__encoding
+
+    @encoding.setter
+    def encoding(self, value):
+        #pylint: disable=missing-docstring
+        if value not in ("text", "json"):
+            raise ValueError("encoding must be 'text' or 'json'")
+        self.__encoding = value
+
+    @property
+    def protocol(self):
+        #pylint: disable=missing-docstring
+        return self.__protocol
+
+    @protocol.setter
+    def protocol(self, value):
+        #pylint: disable=missing-docstring
+        if value not in ("http", "https"):
+            raise ValueError("protocol must be 'http' or 'https'")
+        self.__protocol = value
+
+    @property
+    def timestamps(self):
+        #pylint: disable=missing-docstring
+        return self.__timestamps
+
+    @timestamps.setter
+    def timestamps(self, value):
+        #pylint: disable=missing-docstring
+        self.__timestamps = True if value else False
+
+    @property
+    def uri(self):
+        """generate and return the URI"""
+        if hasattr(self, "__uri"):
+            return self.__uri
+        _uri = "{}://{}:{}/command-api"
+        self.__uri = _uri.format(self.protocol, self.host, self.port)
+        return self.__uri
+
+    def _check_response(self, response):
+        if "error" in response:
+            _message = response["error"]["message"]
+            _code = response["error"]["code"]
+            raise _CapiException("Error [{}]: {}".format(_code, _message))
+
+    def _get_auth_opener(self):
+        """create a URL opener with an authentication handler"""
+        passmgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        passmgr.add_password(None, self.uri, self.user, self.password)
+        handler = urllib2.HTTPBasicAuthHandler(passmgr)
+        opener = urllib2.build_opener(handler)
+        return opener.open
+
+    def _request(self, commands, timestamps=None, encoding=None):
+        """generate the request data"""
+        
+        if not encoding:
+            encoding = self.encoding
+        
+        if not timestamps:
+            timestamps = self.timestamps
+        
+        params = {"version": 1, "cmds": commands, "format": encoding}
+        # timestamps is a newer param, only include it if requested
+        if timestamps:
+            params["timestamps"] = timestamps
+
+        return json.dumps({"jsonrpc": "2.0", "method": "runCmds",
+                           "params": params, "id": self.capi_id})
+
+    def _send(self, data):
+        """sned the request data to the host and return the response"""
+        header = {'Content-Type': 'application/json'}
+        req = urllib2.Request(self.uri, data, header)
+        response = self.http(req)
+        data = json.loads(response.read())
+        self._check_response(data)
+        return data
+    
+    def enable(self):
+        self.__enable = True
+    
+    def execute(self, commands, **kwargs):
+        """execute a series of commands on a remote host"""
+        
+        pop_enable_result = False
+        if self.__enable:
+            # don't overwrite commands
+            commands = list(commands)
+            commands.insert(0, {"cmd": "enable", "input": self.enable_pass})
+            pop_enable_result = True
+
+        _request = self._request(commands, **kwargs)
+        response = self._send(_request)
+
+        if pop_enable_result:
+            response["result"].pop(0)
+        return response
+
+# def main():
+#     """called if directly invoked"""
+#     import sys
+#     capi = _Capi(sys.argv[1], "admin")
+#     # per execute params...
+#     capi.enable()
+#     print capi.execute(sys.argv[2:], encoding="json", timestamps=False)
+#
+# if __name__ == "__main__":
+#     main()
--- arcomm/protocols/capi.py
+++ arcomm/protocols/capi.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-"""Protocol adapter for CAPI aka eAPI aka JSON-RPC"""
 
-import jsonrpclib
-import socket
+from _capi import _Capi, _CapiException
 from ..command import Command
 from ..protocol import Protocol
 from ..exceptions import ExecuteFailed, ConnectFailed
@@ -16,85 +14,53 @@ def _format_commands(commands):
         formatted.append(dict(cmd=_cmd, input=_answer))
     return formatted
 
-# wrapper for jsonrpclib, works with Arista switches over http or https?
 class Capi(Protocol):
-    """Protocol adapter for CAPI aka eAPI aka JSON-RPC"""
-
-    _url = None
-    _format = "text"
-    _transport = "http"
-    _enable = None
+    
     _marker = ">"
-
-    @property
-    def url(self):
-        """Return the internal '_url' attr"""
-        return self._url
-
-    @property
-    def format(self):
-        """Return the internal '_format' attr"""
-        return self._format
-
-    @property
-    def transport(self):
-        """Return the internal '_transport' attr"""
-        return self._transport
-
-    def _seturl(self, host, creds, transport):
-        """Sets the JSON-RPC URL"""
-        url = ("{transport}://{creds.username}:{creds.password}@{host}/"
-               "command-api")
-        url = url.format(host=host, creds=creds, transport=transport)
-        self._url = url
-
+    _use_ssl = False
+    _port = 80
+    _encoding = "text"
+    _timestamps = False
     def _on_initialize(self, **kwargs):
         """Setup protocol specific attributes"""
-
-        transport = kwargs.get("transport") or self._transport
-        self._format = kwargs.get("output_format") or self._format
-        self._seturl(self.host, self.creds, transport)
+        # transport = kwargs.get("transport") or self._transport
+        # self._format = kwargs.get("output_format") or self._format
+        # self._seturl(self.host, self.creds, transport)
 
     def _authorize(self, secret):
         """Authorize the 'session'"""
-        self._enable = Command("enable", answer=secret)
+        self.connection.enable()
         self._marker = "#"
 
     def _connect(self, host, creds):
-        """Make the connection.  Force a test by running 'help' on the switch"""
-
-        try:
-            connection = jsonrpclib.Server(self.url)
-
-            # since JSON-RPC doesn't connect right away, we will run a test
-            # command
-            self._connection = connection
-            self.execute("show clock")
-            return connection
-        except IOError as exc:
-            raise ConnectFailed(exc.message)
-
+        
+        self._conn = _Capi(host, username=creds.username,
+                            password=creds.password,
+                            enable=creds.authorize_password,
+                            use_ssl=self._use_ssl, port=self._port,
+                            encoding=self._encoding, timestamps=self._timestamps)
+        return self._conn
+        
     def _format_response(self, commands, responses):
         """Format the command response to look like an interactive session"""
+
         formatted = []
         # make a fake prompt
         prompt = "{} (command-api){}".format(self.host, self._marker)
+        
         for command, response in zip(commands, responses):
             command = command.get("cmd")
             response = "{}{}\n{}".format(prompt, command, response)
             formatted.append(response)
         return formatted
-
+    
     def _sendall(self, commands):
-        """Sends all commands at once since CAPI doesn't deal with sessions"""
-        if self._enable:
-            commands.insert(0, self._enable)
         commands = _format_commands(commands)
         try:
-            responses = self.connection.runCmds(1, commands, self.format)
-            responses = [_response["output"] for _response in responses]
-            responses = self._format_response(commands, responses)
-            return responses
-        except jsonrpclib.jsonrpc.ProtocolError as exc:
+            response = self.connection.execute(commands)
+        except _CapiException as exc:
             raise ExecuteFailed("Send failed: {}".format(exc.message))
-
+        
+        response = [_res["output"] for _res in response["result"]]
+        response = self._format_response(commands, response)
+        return response
