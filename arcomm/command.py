848e1e770c36ed62837693513045ab32339f5db9
Jesse Mather <jrmather@gmail.com>
2015-04-30 11:12:43 -0700

    rename 'expression' to 'cmd'

--- arcomm/command.py
+++ arcomm/command.py
@@ -7,14 +7,15 @@ from .util import to_list
 class Command(collections.MutableMapping):
     """Object to store command and any prompts or answers it may require"""
 
-    def __init__(self, expression, prompt=None, answer=None):
-
-        assert isinstance(expression, basestring), "expression must be a string"
-
+    def __init__(self, cmd, prompt=None, answer=None):
+        if isinstance(cmd, dict):
+            prompt = cmd.get("prompt")
+            answer = cmd.get("answer")
+            cmd = cmd.get("cmd")
+        assert isinstance(cmd, basestring), "cmd must be a string"
+        
         self._store = dict()
-        prompt = to_list(prompt)
-        updates = dict(expression=expression, prompt=prompt, answer=answer)
-        self.update(updates)
+        self.update(dict(cmd=cmd, prompt=to_list(prompt), answer=answer))
 
     def __getitem__(self, key):
         return self._store[key]
@@ -32,10 +33,10 @@ class Command(collections.MutableMapping):
         return len(self._store)
 
     @property
-    def expression(self):
+    def cmd(self):
         """Returns command sting"""
-        return self._store.get("expression")
-    command = expression
+        return self._store.get("cmd")
+    expression = command = cmd
 
     @property
     def prompt(self):
@@ -48,9 +49,7 @@ class Command(collections.MutableMapping):
         return self._store.get("answer")
 
     def __str__(self):
-        return self.expression
+        return self.cmd
 
     def __repr__(self):
-        _data = dict(expression=self.expression, prompt=self.prompt,
-                     answer=self.answer)
-        return str(_data)
+        return str(dict(cmd=self.cmd, prompt=self.prompt, answer=self.answer))
