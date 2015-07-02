de122e91c9d896966f2eda62d33721430f05b572
Jesse Mather <jrmather@gmail.com>
2015-07-02 13:14:54 -0700

    cleanup vagrant bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-apt-get install -y build-essential python-pip python-paramiko
+apt-get install -y build-essential python-pip python-paramiko python-pytest
 cd /vagrant; python setup.py develop
+
+if ! grep -q vswitch /etc/hosts; then
+
+    cat >> /etc/hosts <<EOF
+192.168.56.21 vswitch1
+192.168.56.22 vswitch2
+EOF
+
+fi
