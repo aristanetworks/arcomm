18856e8abe22d20d9b75784a23e8bbcf191ba5de
Jesse Mather <jrmather@gmail.com>
2015-11-28 21:15:18 -0800

    add more package level imports

--- arcomm/__init__.py
+++ arcomm/__init__.py
@@ -9,7 +9,9 @@ __author__ = 'Jesse Mather'
 
 # pylint: disable=wildcard-import
 from . import util
-from .api import connect, execute, configure
+from .api import connect, execute, configure, background, pool
+from .async import Pool, IterQueue
+#from .cli import Cli
 from .credentials import Creds, BasicCreds
 from .session import session, Session
 from .response import ResponseStore, Response
