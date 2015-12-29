7d2dae7f2c438bf97cbce6d31222a124e4cea913
Jesse Mather <jrmather@gmail.com>
2015-12-29 10:46:55 -0800

    initial import

---
+++ arcomm/protocols/mock.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+import json
+import warnings
+
+from arcomm.exceptions import (ConnectFailed, ExecuteFailed,
+                               AuthenticationFailed,  AuthorizationFailed,
+                               ProtocolException)
+from arcomm.protocols.protocol import BaseProtocol
+from arcomm.command import Command
+
+import platform
+import datetime
+
+def _show_version(authorized):
+    return " ".join(platform.uname())
+
+def _show_clock(authorized):
+    return datetime.datetime.utcnow().isoformat()
+
+def _configure(authorized):
+    pass
+
+def _show_restricted(authorized):
+    if not authorized:
+        raise ValueError('Command not authorized')
+
+    return 'Drink more Ovaltine'
+
+def _end():
+    pass
+
+COMMANDS = {
+# 'show': {
+    #     'version': " ".join(platform.uname()),
+    #     'clock:': datetime.datetime.utcnow().isoformat(),
+    # }
+    'show version': _show_version,
+    'show clock': _show_clock,
+    'show restricted': _show_restricted,
+    'configure': _configure,
+    'end': _end
+}
+
+class Mock(BaseProtocol):
+
+    def __init__(self):
+        # ghost connection
+        self._connected = False
+        self._authorized = False
+
+    def authorize(self, password, username):
+        if 'bad' in password:
+            raise AuthorizationFailed('Invalid authorization')
+
+        self._auhorized = True
+
+    def close(self):
+        self._connected = False
+
+    def connect(self, host, creds, **kwargs):
+        if 'bad' in creds.password or 'bad' in creds.username:
+            raise AuthenticationFailed('Invalid username/password')
+
+        self._connected = True
+
+    def send(self, commands, **kwargs):
+
+        results = []
+
+        for command in commands:
+            if command.cmd in COMMANDS:
+                try:
+                    results.append(COMMANDS[command.cmd](self._authorized))
+                except Exception as exc:
+                    raise ExecuteFailed('Command failed: {}'.format(exc.message))
+            else:
+                raise ExecuteFailed('Invalid command {}'.format(command.cmd),
+                                    command)
+
+        return results
