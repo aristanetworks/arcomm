61265e98455613bcbcf509ffaa281c1837c450d3
Jesse Mather <jrmather@gmail.com>
2016-01-19 09:53:20 -0800

    production version

--- arcomm/__init__.py
+++ arcomm/__init__.py
@@ -5,7 +5,7 @@
 """
 
 __description__ = 'Library for connecting to Arista switches'
-__version__ = '2.0.0-beta.8'
+__version__ = '2.0.0'
 __author__ = 'Jesse Mather'
 __email__ = 'jmather@arista.com'
 __uri__ = 'https://github.com/aristanetworks/arcomm'
--- setup.py
+++ setup.py
@@ -4,52 +4,6 @@ Copyright (c) 2014 Arista Networks, Inc.  All rights reserved.
 Arista Networks, Inc. Confidential and Proprietary.
 """
 
-# import sys
-# import os
-# from setuptools import setup, find_packages
-#
-# if sys.version_info < (2, 7, 0):
-#     raise NotImplementedError("Sorry, you need at least Python 2.7 to install.")
-#
-# sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
-# from arcomm import __version__
-#
-# readme = open('README.rst').read()
-#
-# setup(
-#     name = "arcomm",
-#     version = __version__,
-#     author = "Jesse Mather",
-#     author_email = "jmather@arista.com",
-#     maintainer=
-#     maintainer_email=
-#     description = "Library for connecting to Arista switches",
-#     long_description = readme,
-#     classifiers = [
-#         "Development Status :: 3 - Alpha",
-#         "Environment :: Console",
-#         "Intended Audience :: Information Technology",
-#         "License :: OSI Approved :: MIT License",
-#         "Natural Language :: English",
-#         "Operating System :: OS Independent",
-#         "Programming Language :: Python",
-#         "Programming Language :: Python :: 2.7",
-#         "Topic :: Software Development :: Libraries :: Python Modules",
-#         "Topic :: Software Development :: Testing",
-#         "Topic :: Terminals"
-#     ],
-#     packages = find_packages(),
-#     install_requires = ["requests"],
-#     url = "https://github.com/aristanetworks/arcomm",
-#     license = "MIT Licesnse",
-#     entry_points = {
-#         'console_scripts': [
-#             'arcomm = arcomm.entry:main',
-#         ]
-#     }
-# )
-
-
 import codecs
 import os
 import re
@@ -64,7 +18,7 @@ PACKAGES = find_packages()
 META_PATH = os.path.join(NAME, '__init__.py')
 KEYWORDS = ['arista', 'eapi', 'ssh']
 CLASSIFIERS = [
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 5 - Production/Stable',
     'Environment :: Console',
     'Intended Audience :: Information Technology',
     'License :: OSI Approved :: MIT License',
