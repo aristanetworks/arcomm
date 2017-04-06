7f5814498d56533f85bf02721278fa5f7cb716be
Jesse Mather <jrmather@gmail.com>
2017-04-06 16:22:39 -0700

    define arcomm configuration directory

--- arcomm/env.py
+++ arcomm/env.py
@@ -6,6 +6,7 @@
 """
 read env variables or use sensible defaults
 """
+import os
 
 ARCOMM_DEFAULT_PROTOCOL = 'eapi+http'
 ARCOMM_DEFAULT_TIMEOUT  = 30
@@ -13,3 +14,11 @@ ARCOMM_DEFAULT_USERNAME = 'admin'
 ARCOMM_DEFAULT_PASSWORD = ''
 ARCOMM_DEFAULT_SUPER = ''
 ARCOMM_DEFAULT_SUPASS = ''
+
+if os.name == 'nt':
+    ARCOMM_CONF_DIR = os.path.join(os.getenv('APPDATA'), 'arcomm')
+else:
+    # this will apply to both posix and java
+    ARCOMM_CONF_DIR = os.path.expanduser('~/.arcomm')
+
+ARCOMM_SECRETS_FILE = os.path.join(ARCOMM_CONF_DIR, 'secrets.yml')
