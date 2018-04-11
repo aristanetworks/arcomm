09e19336f0f62f32c45440df54a576cf17a118b4
Jesse Mather <jmather@arista.com>
2018-04-11 11:48:48 -0700

    version bump

--- arcomm/__init__.py
+++ arcomm/__init__.py
@@ -5,8 +5,8 @@
 
 import sys
 
-__version_info__ = (2, 2, 11)
-__version__ = '2.2.11'
+__version_info__ = (2, 2, 12)
+__version__ = '2.2.12'
 
 if sys.version_info < (2, 7):
     raise RuntimeError('You need Python 2.7+ for arcomm.')
